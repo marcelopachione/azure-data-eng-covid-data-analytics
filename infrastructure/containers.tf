83772fde9abe987157f54a86fc9f812c7b9eabd7
Marcelo Ribas <marcelo.pachione@icloud.com>
2023-08-24 14:27:55 +0100

    Add infra code using terraform

---
+++ infrastructure/containers.tf
@@ -0,0 +1,28 @@
+# Create Datalake ##
+
+# Create directory raw-data
+resource "azurerm_storage_data_lake_gen2_filesystem" "fc" {
+  name               = "lab-covid-brazil-240823"
+  storage_account_id = azurerm_storage_account.sa.id
+}
+
+resource "azurerm_storage_data_lake_gen2_path" "fc-bronze" {
+  path               = "bronze"
+  filesystem_name    = azurerm_storage_data_lake_gen2_filesystem.fc.name
+  storage_account_id = azurerm_storage_account.sa.id
+  resource           = "directory"
+}
+
+resource "azurerm_storage_data_lake_gen2_path" "fc-silver" {
+  path               = "silver"
+  filesystem_name    = azurerm_storage_data_lake_gen2_filesystem.fc.name
+  storage_account_id = azurerm_storage_account.sa.id
+  resource           = "directory"
+}
+
+resource "azurerm_storage_data_lake_gen2_path" "fc-gold" {
+  path               = "gold"
+  filesystem_name    = azurerm_storage_data_lake_gen2_filesystem.fc.name
+  storage_account_id = azurerm_storage_account.sa.id
+  resource           = "directory"
+}
